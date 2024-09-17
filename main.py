fcd77c2037de4cb52d04aaa53ad4716696ca1bf7
Sb-gabriel <gabrielsanchez@icea.edu.ar>
2024-09-17 01:04:00 +0000

    test

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-'''
-'''
+#tipos de datos:
+numeroEntero=int(1)
+numeroDecimal=float(3.14)
+booleano=bool(True)
+string=str('cadena de caracteres alfanumericos o texto')
+for x in range(21):
+    print(x)
+boolean=True
+
+while boolean:
+    x-=1
+    boolean=x>0
+    print(x)
+
+if x==0:
+    print('x es igual a 0')
+
+def funcion(parametro):
+    valorRetornado=0
+    return valorRetornado
